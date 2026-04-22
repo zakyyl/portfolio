8c9a08255620938299c492687f0f6d2c3fb0efa9
zakyyl <zaki24112003@gmail.com>
2026-04-22 10:23:28 +0700

    refact data funct

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,16 +1,16 @@
 export const experiences = [
   {
-    title: "IT Programmer",
-    company: "RS Bhayangkara Jambi",
-    year: "2025 - 2026",
-    description:
-      "Developing and maintaining hospital management systems, focusing on patient data management and medical records.",
-    image: "/images/experiences/unnamed.webp",
-  },
-  {
-    title: "Assistant Laboratorium",
+    title: "Study In Universitas Dinamika Bangsa",
     company: "Universitas Dinamika Bangsa",
-    year: "2022 - 2024",
+    year: "2021 - 2025",
+    description:
+      "Built responsive web applications using Laravel and React for various clients.",
+    image: "/images/experiences/unamak.jpg",
+  },
+    {
+    title: "Assistant Laboratorium",
+    company: "Assistant Laboratorium",
+    year: "2021 - 2025",
     description:
       "Built responsive web applications using Laravel and React for various clients.",
     image: "/images/experiences/aslab.png",
@@ -24,6 +24,30 @@ export const experiences = [
     image: "/images/experiences/stupen.png",
   },
   {
+    title: "Academic Seminar Operator",
+    company: "Academic Seminar Operator",
+    year: "2023 - 2025",
+    description:
+      "Provided technical support for academic paper seminars, handling audiovisual systems, supporting presenters, and maintaining smooth seminar operations.",
+    image: "/images/experiences/opeyator.jpeg",
+  },
+  {
+    title: "Orientation Committee",
+    company: "Orientation Committee",
+    year: "2023 - 2025",
+    description:
+      "Served as a committee member for student orientation, coordinating activities, assisting new students, and ensuring smooth event execution.",
+    image: "/images/experiences/platinum.png",
+  },
+  {
+    title: "Graduation Committee",
+    company: "Graduation Committee",
+    year: "2023 - 2025",
+    description:
+      "Participated as a graduation committee member, supporting event preparation, managing logistics, and ensuring the graduation ceremony ran smoothly.",
+    image: "/images/experiences/wisuda.png",
+  },
+  {
     title: "BootCamp - FullStack Developer",
     company: "Celerates Academy",
     year: "2024 - 2025",
@@ -32,27 +56,11 @@ export const experiences = [
     image: "/images/experiences/celerates.jpg",
   },
   {
-    title: "Academic Seminar Operator",
-    company: "Universitas Dinamika Bangsa",
-    year: "2023 - 2025",
+    title: "IT Programmer",
+    company: "RS Bhayangkara Jambi",
+    year: "2025 - 2026",
     description:
-      "Provided technical support for academic paper seminars, handling audiovisual systems, supporting presenters, and maintaining smooth seminar operations.",
-    image: "/images/experiences/platinum.png",
-  },
-  {
-    title: "Orientation Committee",
-    company: "Universitas Dinamika Bangsa",
-    year: "2023 - 2025",
-    description:
-      "Served as a committee member for student orientation, coordinating activities, assisting new students, and ensuring smooth event execution.",
-    image: "/images/experiences/platinum.png",
-  },
-  {
-    title: "Graduation Committee",
-    company: "Universitas Dinamika Bangsa",
-    year: "2023 - 2025",
-    description:
-      "Participated as a graduation committee member, supporting event preparation, managing logistics, and ensuring the graduation ceremony ran smoothly.",
-    image: "/images/experiences/wisuda.png",
+      "Developing and maintaining hospital management systems, focusing on patient data management and medical records.",
+    image: "/images/experiences/unnamed.webp",
   },
 ];
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,19 +1,12 @@
 export const projects = [
-  {
-    title: "Flutter UI E-Commerce",
-    description:
-      "Mobile Online Web E-Commerce.",
-    tech: ["Flutter"],
-    github: "https://github.com/username/hospital-system",
-    image: "/images/projects/ecom.png", 
-  },
+
 
   {
     title: "Keluh Prov",
     description:
       "Platform For Reporting Provincial Issues.",
     tech: ["Flutter"],
-    github: "https://github.com/username/medical-dashboard",
+    github: "https://github.com/Capstone-Project-Alterra-Kelompok-8/e-complaint-mobile.git",
     image: "/images/projects/keluhprov.png",
   },
 
@@ -22,7 +15,7 @@ export const projects = [
     description:
       "Mobile Application For Dental Clinic Management.",
     tech: ["Flutter"],
-    github: "https://github.com/username/medical-dashboard",
+    github: "https://github.com/zakyyl/Flutter_Komi_Dentist_MiniProject.git",
     image: "/images/projects/komidentist.png", 
   },
     {
@@ -30,23 +23,33 @@ export const projects = [
     description:
       "Flutter UI Clother Store.",
     tech: ["Flutter"],
-    github: "https://github.com/username/medical-dashboard",
+    github: "https://github.com/zakyyl/flutter-ui-e-commerce.git",
     image: "/images/projects/clothestore.png", 
   },
+
     {
-    title: "HTML Landing Page",
+    title: "Flutter UI E-Commerce",
+    description:
+      "Mobile Online Web E-Commerce.",
+    tech: ["Flutter"],
+    github: "https://github.com/zakyyl/flutter-ui-e-commerce.git",
+    image: "/images/projects/ecom.png", 
+  },
+
+    {
+    title: "Landing Page Clothes Store",
     description:
       "HTML UI Landing Page.",
     tech: ["HTML"],
-    github: "https://github.com/username/medical-dashboard",
+    github: "https://github.com/zakyyl/hakara-clothes.git",
     image: "/images/projects/hclothes.png", 
   },
     {
-    title: "PMB ACAI",
+    title: "Portal Penerimaan Mahasiswa Baru",
     description:
       "Website Aplikasi Penerimaan Mahasiswa.",
     tech: ["Laravel"],
-    github: "https://github.com/username/medical-dashboard",
+    github: "https://github.com/zakyyl/project_pmb_acai.git",
     image: "/images/projects/pmbacai.png", 
   },
   {
@@ -54,7 +57,7 @@ export const projects = [
     description:
       "Digitalisasi Menggunakan Website Untuk Adopsi Kucing.",
     tech: ["Laravel"],
-    github: "https://github.com/username/medical-dashboard",
+    github: "https://github.com/zakyyl/frontend_kucing",
     image: "/images/projects/pawfect.jpg", 
   },
   {
@@ -62,15 +65,31 @@ export const projects = [
     description:
       "Aplikasi Pemesan Layanan Kebersihan Rumah.",
     tech: ["Flutter"],
-    github: "https://github.com/username/medical-dashboard",
+    github: "https://github.com/zakyyl/homecleaning",
     image: "/images/projects/homecare.png", 
   },
   {
     title: "Indeks Kepuasaan Pasien",
     description:
       "Aplikasi Kuesioner Kepuasan Pasien.",
-    tech: ["Flutter"],
-    github: "https://github.com/username/medical-dashboard",
+    tech: ["Laravel"],
+    github: "https://github.com/kurniagustino/survey_pasien.git",
     image: "/images/projects/ikape.jpg", 
+  },
+  {
+    title: "Clinical Pathway",
+    description:
+      "Aplikasi Kuesioner Kepuasan Pasien.",
+    tech: ["Laravel"],
+    github: "https://github.com/zakyyl/medical-dashboard",
+    image: "/images/projects/cp.png", 
+  },
+    {
+    title: "GymOS",
+    description:
+      "Aplikasi Management GYM.",
+    tech: ["Laravel"],
+    github: "https://github.com/zakyyl/gym",
+    image: "/images/projects/gym.png", 
   },
 ];
